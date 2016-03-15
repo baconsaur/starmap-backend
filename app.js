c25ce2503622b990656d35cbdf24c2a8a773ff02
Ash Kestrel <tetrapteryxgames@gmail.com>
2016-03-15 11:07:20 -0600

    :pineapple: Preparing to deploy

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(cors());
+app.use(cors({
+	origin: process.env.DEPLOYED_URL || true
+}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
